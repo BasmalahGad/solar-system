5453ce92715fcfa6170390f283740049ef7241f4
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2025-08-17 23:53:05 +0300

    pipline test

--- infrastructure/02- main.tf
+++ infrastructure/02- main.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-basmala-1311"
+    key            = "solar-system-eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-table"
+    encrypt        = true
+  }
+}
+
 module "vpc" {
   source = "./modules/vpc"
 
